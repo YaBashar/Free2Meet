305c7be707544e531bc4254b1a1d33b0d8a4ef88
MUBASHIR MUHAMMAD HUSSAIN <z5599894@ad.unsw.edu.au>
2025-08-19 22:50:14 +1000

    Created middleware to verify jwt access tokens

---
+++ backend/src/middleware.ts
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const SECRET = process.env.JWT_SECRET;
+
+import {Request, Response, NextFunction} from 'express'
+
+const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+    const authHeaders = req.headers['authorization']
+    if(!authHeaders) return res.status(401).json({error: "Unauthorised: No token proivded"});
+
+    const token = authHeaders.split(' ')[1];
+    if(!token) return res.status(401).json({error: "Unauthorised: Malformed JWT Token"});
+
+    try {
+        const decoded = jwt.verify(token, SECRET);  
+        (req as any).userId = decoded.userId;
+        next();
+
+    } catch (error) {
+        return res.status(401).json({error: "Unauthorised"});
+    }
+}
+
+export {verifyJWT}
