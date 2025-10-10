9ec8bb9b7bc8b808d579b678569785640d19d130
Mubashir Hussain <mubashirmh04@gmail.com>
2025-10-11 00:20:41 +1100

    Updated all components with new color scheme and UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+            'white': '#FFFFFF',
+            'ghost-white': '#ECEEF9',
+            'lavender': '#D8DDF3',
+            'vista-blue': '#8C99D9',
+            'byzantine-blue': '#4055BF',
+            'marian-blue': '#2D3C86',
+            'delft-blue': '#202B60'
+          }
+    },
   },
   plugins: [],
 }
