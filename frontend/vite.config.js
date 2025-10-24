9406eb4d24395652d04795c7cff9c42d68a36bba
Mubashir Hussain <mubashirmh04@gmail.com>
2025-10-24 22:38:39 +1100

    updating tailwind 3 to 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import { fileURLToPath } from 'url';
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     port: 3000
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   }
 })
