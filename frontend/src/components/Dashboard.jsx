f693d84ee84e06f69680d5b02041316e5dd13190
Mubashir Hussain <mubashirmh04@gmail.com>
2025-10-25 23:11:03 +1100

    Created main dashboard to create view and edit events

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,33 +1,35 @@
-import { useAxiosPrivate } from '../hooks/useAxiosPrivate';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { DialogDemo } from './Dialog'
 
 const Dashboard = () => {
     
-    const USER_URL = '/auth/user-details'
-    const axiosPrivate = useAxiosPrivate();
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    const handleClick = async() => {
-
-        try {
-            const response = await axiosPrivate.get(USER_URL);
-            console.log(JSON.stringify(response));
-
-        } catch (err) {
-            console.log(JSON.stringify(err.response.data.error));
-            navigate('/login', { state: { from: location }, replace: true})
-        }
-    }
-
-    
-
   return (
     <>
-        <div className="text-red-400">Dashboard</div>
-        <button onClick={handleClick}>Click Me</button>
-    </>
-    
+        <div className = "flex flex-col m-3 w-[1090px] rounded-xl frosted">
+            <h1 className="ml-5 mt-2 mb-2 text-xl text-black text-[30px] text-">Welcome Back</h1>
+
+            <div className='w-full'>
+                <h1 className="ml-5 text-black">My Events</h1>
+
+                <div className= "flex flex-row flex-wrap ml-5 justify-start ">
+                    <div className="w-[150px] h-[150px] bg-red-100 m-5 flex rounded-xl justify-center items-center text-black">
+                        <DialogDemo></DialogDemo>
+                    </div>
+                    
+                    <div className="w-[150px] h-[150px] bg-black m-5 rounded-xl"></div>
+                    <div className="w-[150px] h-[150px] bg-red-500 m-5 rounded-xl"></div>
+                </div>
+            </div>
+
+            <div className='w-full'>
+                <h1 className="ml-5 text-black">Attending Events</h1>
+                <div className= "flex flex-row flex-wrap ml-5">
+                    <div className="w-[150px] h-[150px] bg-red-100 m-5 rounded-xl"></div>
+                    <div className="w-[150px] h-[150px] bg-red-100 m-5 rounded-xl"></div>
+                    <div className="w-[150px] h-[150px] bg-red-100 m-5 rounded-xl"></div>   
+                </div>
+            </div>
+        </div>
+    </> 
   )
 }
 
