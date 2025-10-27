a20d1b309ee5294a75c0013dae8138d0f7ed0d6b
Mubashir Hussain <mubashirmh04@gmail.com>
2025-10-27 16:04:53 +1100

    Updated AuthProvider and removed unnecessary functions

--- frontend/src/hooks/useRefreshToken.js
+++ frontend/src/hooks/useRefreshToken.js
@@ -1,17 +1,14 @@
 import axios from '../api/axios'
-import { useAuth } from './useAuth'
 
 const useRefreshToken = () => {
 
-    const { signIn } = useAuth();
-  
     const refresh = async() => {
         const response = await axios.get('/auth/refresh', {
             withCredentials: true
         });
 
         const accessToken = response.data.token;
-        signIn(accessToken);
+        return accessToken;
     }
   
     return refresh;
