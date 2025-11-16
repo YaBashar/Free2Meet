f59e56d6cd47c761ef05e86ba86f506a54e0fa55
Mubashir Hussain <mubashirmh04@gmail.com>
2025-11-16 21:51:57 +1100

    Created tests for notAttending endpoint

---
+++ backend/src/tests/eventTests/userGetNotAttendingEvent.test.ts
@@ -0,0 +1,57 @@
+import { requestAttendeeRespond, requestAuthLogin, requestAuthRegister, requestDelete, requestEventInvite, requestNewEvent, requestNotAttendingEvent } from '../requestHelpers';
+
+let organiserToken: string;
+let attendeeToken: string;
+let link : string;
+let eventId: string;
+
+beforeEach(() => {
+  requestDelete();
+
+  requestAuthRegister('Mubashir', 'Hussain', 'Abcdefg123$', 'example@gmail.com');
+  const res = requestAuthLogin('example@gmail.com', 'Abcdefg123$');
+  const data = JSON.parse(res.body.toString());
+  organiserToken = data.token;
+
+  const res1 = requestNewEvent(organiserToken, 'New Event', 'New Description', 'House', '31/08/2025', 10, 14);
+  const data1 = JSON.parse(res1.body.toString());
+  eventId = data1.eventId;
+
+  const res2 = requestEventInvite(organiserToken, eventId);
+  const data2 = JSON.parse(res2.body.toString());
+  link = data2.link;
+
+  requestAuthRegister('Jonathan', 'Lee', 'Abcnmop.123$', 'jonl@gmail.com');
+  const res3 = requestAuthLogin('jonl@gmail.com', 'Abcnmop.123$');
+  const data3 = JSON.parse(res3.body.toString());
+  attendeeToken = data3.token;
+});
+
+afterEach(() => {
+  requestDelete();
+});
+
+describe(('Error'), () => {
+  test('Invalid Event Id', () => {
+    requestAttendeeRespond(attendeeToken, link, 'reject');
+    const res = requestNotAttendingEvent('invalid');
+    const data = JSON.parse(res.body.toString());
+    expect(res.statusCode).toStrictEqual(400);
+    expect(data).toStrictEqual({ error: expect.any(String) });
+  });
+});
+
+describe(('Success'), () => {
+  test('Success', () => {
+    requestAttendeeRespond(attendeeToken, link, 'reject');
+    const res = requestNotAttendingEvent(eventId);
+    const data = JSON.parse(res.body.toString());
+    expect(res.statusCode).toStrictEqual(200);
+    expect(data).toStrictEqual([
+      {
+        name: 'Jonathan Lee',
+        declinedAt: expect.any(String)
+      }
+    ]);
+  });
+});
