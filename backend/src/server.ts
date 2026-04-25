9941d245dfd4e39b26341d9c07653f17d4b9a1d8
Mubashir Hussain <mubashirmh04@gmail.com>
2026-04-25 13:26:15 +1000

    fix: update to connect to db before connecting to server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import config from './config.json';
 import { app } from './app';
+import { Server } from "http";
 import mongoose from 'mongoose';
 
 const PORT: number = parseInt(process.env.PORT || config.port);
@@ -7,18 +8,29 @@ const HOST: string = (process.env.IP || '127.0.0.1');
 
 // ====================================================================
 // ====================================================================
+let server: Server | null = null;
+
 mongoose.connect(process.env.MONGODB_URI, {
 })
-  .then(() => console.log('DB Connection Successful'));
+  .then(() => {
+    console.log('DB Connection Successful')
+    server = app.listen(PORT, HOST, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  });
 
-const server = app.listen(PORT, HOST, () => {
-  console.log(`Server running on ${PORT}`);
-});
 
 // For coverage, handle Ctrl+C gracefully
-process.on('SIGINT', () => {
-  server.close(() => {
-    console.log('Shutting down server gracefully.');
-    process.exit();
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, () => {
+    if (!server) {
+      process.exit(0);
+    }
+
+    server.close(() => {
+      console.log("Server closed Gracefully");
+      process.exit();
+    });
   });
 });
+
