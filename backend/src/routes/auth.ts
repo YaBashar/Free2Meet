3b6f7f278c62db0341d48a69049ef191ec6566e0
Mubashir Hussain <mubashirmh04@gmail.com>
2026-04-25 13:08:26 +1000

    refactor: update middleware funcion name from verifyJwt to requireAuth

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,14 +1,21 @@
-import express from 'express';
-import { verifyJWT } from '../middleware';
-import * as authController from '../controllers/auth.controller';
+import express from "express";
+import { requireAuth } from "../middleware";
+import * as AuthController from "../controllers/auth.controller";
 
 export const authRouter = express.Router();
 
-authRouter.post('/register', authController.register);
-authRouter.post('/login', authController.login);
-authRouter.get('/refresh', authController.refresh);
-authRouter.post('/request-reset', authController.requestReset);
-authRouter.post('/reset-password', authController.resetPassword);
-authRouter.get('/user-details', verifyJWT, authController.userInfo);
-authRouter.post('/logout', verifyJWT, authController.logout);
-authRouter.put('/change-password', verifyJWT, authController.changePassword);
+authRouter.post("/register", AuthController.register);
+authRouter.post("/login", AuthController.login);
+authRouter.post("/refresh", AuthController.refresh);
+
+// Forgot Password Flow
+authRouter.post("/forgot-password", AuthController.forgot);
+authRouter.post("/resend-reset-code", AuthController.resendResetCode);
+authRouter.post("/verify-reset-code", AuthController.verifyResetCode);
+authRouter.post("/reset-password", AuthController.resetPassword);
+
+// Email Verification Flow
+authRouter.post("/verify-email", AuthController.verifyEmail);
+authRouter.post("/resend-verification", AuthController.resendVerifyEmail);
+
+authRouter.post("/logout", requireAuth, AuthController.logout);
--- backend/src/routes/event.ts
+++ backend/src/routes/event.ts
@@ -1,16 +1,16 @@
 import express from 'express';
-import { verifyJWT } from '../middleware';
+import { requireAuth } from '../middleware';
 import * as eventController from '../controllers/event.controller';
 
 export const eventRouter = express.Router();
 
-eventRouter.post('/new-event', verifyJWT, eventController.create);
-eventRouter.get('/organised-events', verifyJWT, eventController.organisedEvents);
-eventRouter.get('/attending-events', verifyJWT, eventController.allAttendingEventsForUser);
-eventRouter.post('/:eventId/invite', verifyJWT, eventController.invite);
+eventRouter.post('/new-event', requireAuth, eventController.create);
+eventRouter.get('/organised-events', requireAuth, eventController.organisedEvents);
+eventRouter.get('/attending-events', requireAuth, eventController.allAttendingEventsForUser);
+eventRouter.post('/:eventId/invite', requireAuth, eventController.invite);
 eventRouter.get('/invite/:inviteLink', eventController.inviteDetails);
-eventRouter.put('/:eventId', verifyJWT, eventController.update);
-eventRouter.get('/:eventId', verifyJWT, eventController.info);
+eventRouter.put('/:eventId', requireAuth, eventController.update);
+eventRouter.get('/:eventId', requireAuth, eventController.info);
 eventRouter.get('/:eventId/attending', eventController.getEventAttendees);
 eventRouter.get('/:eventId/notAttending', eventController.notAttending);
-eventRouter.delete('/:eventId', verifyJWT, eventController.remove);
+eventRouter.delete('/:eventId', requireAuth, eventController.remove);
