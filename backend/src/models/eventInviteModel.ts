675772f2d9058629ae12209027ed6e6690afd424
Mubashir Hussain <mubashirmh04@gmail.com>
2026-05-06 18:02:12 +1000

    refactor: use participant model to link user and events

--- backend/src/models/eventInviteModel.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { EventInvite } from './interfaces';
-
-const eventInviteSchema = new mongoose.Schema({
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' },
-  link: { type: String }
-});
-
-export const EventInviteModel = mongoose.model<EventInvite>('EventInvite', eventInviteSchema);
