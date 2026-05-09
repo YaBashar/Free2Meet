949beb5d9cfee14400b140b1e19fcaa8233aac63
Mubashir Hussain <mubashirmh04@gmail.com>
2026-05-09 18:56:08 +1000

    models: update to reflect eventType changes

--- backend/src/models/eventParticipantModel.ts
+++ backend/src/models/eventParticipantModel.ts
@@ -1,4 +1,5 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+
 
 export type EventParticipantStatus = "Pending" | "Accepted" | "Declined";
 
@@ -6,24 +7,24 @@ export interface EventParticipant extends Document {
   userId: string;
   eventId: string;
   name: string;
-  startAvailable?: number;
-  endAvailable?: number;
+  availability?: { date: string; startAvailable: number; endAvailable: number }[];
+  preferredDates?: string[];
   status: EventParticipantStatus;
   inviteCode?: string;
   inviteCodeExpiry?: Date;
-  role: "Organiser" | "Attendee"
+  role: "Organiser" | "Attendee";
 }
 
 const eventParticipantSchema = new mongoose.Schema({
   userId: { type: String, index: true },
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', index: true },
+  eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event", index: true },
   name: { type: String },
-  startAvailable: { type: Number },
-  endAvailable: { type: Number },
-  status: { type: String, enum: ['Pending', 'Accepted', 'Declined']},
+  availability: [{ date: String, startAvailable: Number, endAvailable: Number }],
+  preferredDates: [{ type: String }],
+  status: { type: String, enum: ["Pending", "Accepted", "Declined"] },
   inviteCode: { type: String },
   inviteCodeExpiry: { type: Date },
-  role: { type: String, enum: ['Organiser', 'Attendee']}
+  role: { type: String, enum: ["Organiser", "Attendee"] },
 });
 
-export const EventParticipantModel = mongoose.model('Attendee', eventParticipantSchema);
+export const EventParticipantModel = mongoose.model("Attendee", eventParticipantSchema);
